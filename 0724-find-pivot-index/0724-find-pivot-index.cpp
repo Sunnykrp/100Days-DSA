cac3e35c60d41f7a43b02aac566bd006f73ede9d
Sunny Kumar <108024376+Sunnykrp@users.noreply.github.com>
2024-04-09 22:36:39 +0530

    Time: 12 ms (87.55%), Space: 34 MB (37.23%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+  int pivotIndex(vector<int>& nums) {
+    int n= nums.size();
+     int sum=0;
+     for(int i=0;i<n;i++){
+         sum=sum+nums[i];
+     }int firstsum=0;int secondsum;
+     for(int i=0;i<n;i++){
+     secondsum=sum-nums[i]-firstsum;
+     if(firstsum==secondsum){
+         return i;
+     }
+
+     firstsum=firstsum+nums[i];
+     }
+    return -1;
+    }
+};
